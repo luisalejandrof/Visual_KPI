5defaf45b3939b24c5db5ed80f905000acd79e38
Luis Alejandro <75645866+luisalejandrof@users.noreply.github.com>
2021-03-25 21:53:26 -0300

    Update kpi.py

--- kpi.py
+++ kpi.py
@@ -1,7 +1,13 @@
 # Visual_KPI para revisar datos y visualizarlos
-import pandas as pd
-import matplotlib as pl
+import numpy as np
 #importar libreria matplotlib
-import matplotlib as pl
-# Importar data como dataframe
-data = pd.DataFrame (pd.read_csv('datos.csv'))
+import matplotlib.pyplot as plt
+dx = 0.01; dy = 0.01
+x = np.arange(-2.0,2.0,dx)
+y = np.arange(-2.0,2.0,dy)
+X,Y = np.meshgrid(x,y)
+def f(x,y):
+    return (1 - y**5 + x**5)*np.exp(-x**2-y**2)
+C = plt.contour(X,Y,f(X,Y),8,colors='black')
+plt.contourf(X,Y,f(X,Y),8)
+plt.clabel(C, inline=1, fontsize=10)
